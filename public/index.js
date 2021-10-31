c7699985d190301aa252620411c3923372e8ac32
jhersbo <jhersbo@gmail.com>
2021-10-31 12:15:22 -0500

    progress

--- public/index.js
+++ public/index.js
@@ -4,6 +4,11 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+    let request = fetch('https://api.twelvedata.com/time_series?symbol=AAPL,EUR/USD,ETH/BTC:Huobi,TRP:TSX&interval=1min&apikey=ab290298d975455fab819a7367cfea62',{
+        method: 'GET'
+        
+    })
+
 }
 
 main()
